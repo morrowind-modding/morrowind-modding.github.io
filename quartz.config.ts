bc9e3e44299116e2860e343004d7c71c5c4ad37c
MasssiveJuice <nicholas.h.rowell@gmail.com>
2024-06-26 21:52:15 +1200

    Add `Plugin.HardLineBreaks()` to `quartz.config.ts`

--- quartz.config.ts
+++ quartz.config.ts
@@ -69,6 +69,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.HardLineBreaks(),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
