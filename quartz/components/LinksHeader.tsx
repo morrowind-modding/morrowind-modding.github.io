e2c699f26086835ef4e8777f39c957a3e38b32fe
MasssiveJuice <nicholas.h.rowell@gmail.com>
2024-07-08 17:42:34 +1200

    Fix: correct link destinations
    Added hyphens to `<a>` link destinations containing spaces

--- quartz/components/LinksHeader.tsx
+++ quartz/components/LinksHeader.tsx
@@ -12,27 +12,27 @@ export default (() => {
         <div id="links-header">
           <span>
             <img src=""></img>
-            <a href="/Guides/index">Guides</a>
+            <a href="/Guides/">Guides</a>
           </span>
           <span>
             <img src=""></img>
-            <a href="/Modding Tools/index">Modding Tools</a>
+            <a href="/Modding-Tools/">Modding Tools</a>
           </span>
           <span>
             <img src=""></img>
-            <a href="/TES3 Construction Set/index">Construction Set</a>
+            <a href="/TES3-Construction-Set/">Construction Set</a>
           </span>
           <span>
             <img src=""></img>
-            <a href="/Programmers Reference/index">Programmers Reference</a>
+            <a href="/Programmers-Reference/">Programmers Reference</a>
           </span>
           <span>
             <img src=""></img>
-            <a href="/Programmers Reference/index">Projects</a>
+            <a href="/Projects/">Projects</a>
           </span>
           <span>
             <img src=""></img>
-            <a href="/MMW Design & Planning/index">Contributing</a>
+            <a href="/MMW-Design--and--Planning/">Contributing</a>
           </span>
         </div>
       <hr style="background-color: var(--gray); border-top: 1px var(--gray) solid; margin-top: 1.3rem"></hr>
