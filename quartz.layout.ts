0aa692251c1cb736a1729dcfe80d329233d41592
MasssiveJuice <nicholas.h.rowell@gmail.com>
2024-07-17 18:43:57 +1200

    Fix: Reinstate horizontal navbar
    Accidentally deleted navbar when adding commit 6c36e34 from Quart v4, which removed `header: [Component.LinksHeader()]` from `quartz.layout.ts` and replaced it with `header: []`
    
    Also updated link `href`s to align with new filenames from commit 67258c1

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
+  header: [Component.LinksHeader()],
   afterBody: [],
   footer: Component.Footer({
     links: {
