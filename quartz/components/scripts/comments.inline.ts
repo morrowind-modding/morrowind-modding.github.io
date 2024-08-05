60efaaf80ef9f3485bc3a6a0a14456c1f8e4d3fc
Aaron Pham <Aaronpham0103@gmail.com>
2024-08-05 15:17:11 -0400

    refactor(comments): move script to files (#1308)
    * refactor(comments): move script to files
    
    for LSP, treesitter, and the whole galore.
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    * fix(type): support removeEventListener with CustomEventMap
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    * fix: parse bool to string first
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    * chore: address comments and test on branch
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    * revert: remove comments section from main quartz pages
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>
    
    ---------
    
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- quartz/components/scripts/comments.inline.ts
+++ quartz/components/scripts/comments.inline.ts
@@ -1,57 +1,67 @@
-document.addEventListener("themechange", (e) => {
-  const theme = e.detail.theme === 'light' ?  'light_protanopia' : 'dark_protanopia'
-
-  function sendMessage(message: { setConfig: { theme: string } }) {
-    const iframe = document.querySelector('iframe.giscus-frame') as HTMLIFrameElement;
-    if (!iframe) return;
-    iframe.contentWindow?.postMessage({ giscus: message }, 'https://giscus.app');
+const changeTheme = (e: CustomEventMap["themechange"]) => {
+  const theme = e.detail.theme
+  const iframe = document.querySelector("iframe.giscus-frame") as HTMLIFrameElement
+  if (!iframe) {
+    return
   }
 
-  sendMessage({
-    setConfig: {
-      theme: theme
-    }
-  });
-})
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  const userPref = window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"
-  const currentTheme = localStorage.getItem("theme") ?? userPref
-  if (currentTheme === "dark") {return} // default is dark theme
-
-  const theme = currentTheme === "light" ? "light_protanopia" : "dark_protanopia"
-
-  const existingGiscusContainer = document.getElementById('giscus-container');
-
-  if (existingGiscusContainer) {
-    // Remove the existing Giscus instance
-    existingGiscusContainer.innerHTML = '';
+  if (!iframe.contentWindow) {
+    return
   }
 
-  // Create a new container element for Giscus
-  const newGiscusContainer = document.createElement('div');
-  newGiscusContainer.id = 'giscus-container';
-  document.body.appendChild(newGiscusContainer);
+  iframe.contentWindow.postMessage(
+    {
+      giscus: {
+        setConfig: {
+          theme: theme,
+        },
+      },
+    },
+    "https://giscus.app",
+  )
+}
 
-  // Create a new script element with the updated data-theme attribute
-  const newScript = document.createElement('script');
-  newScript.src = 'https://giscus.app/client.js';
-  newScript.setAttribute('data-repo', 'xy-241/CS-Notes');
-  newScript.setAttribute('data-repo-id', 'R_kgDOK0yNDg');
-  newScript.setAttribute('data-category', 'General');
-  newScript.setAttribute('data-category-id', 'DIC_kwDOK0yNDs4CbdOh');
-  newScript.setAttribute('data-mapping', 'pathname');
-  newScript.setAttribute('data-strict', '0');
-  newScript.setAttribute('data-reactions-enabled', '1');
-  newScript.setAttribute('data-emit-metadata', '0');
-  newScript.setAttribute('data-input-position', 'top');
-  newScript.setAttribute('data-theme', theme);
-  newScript.setAttribute('data-lang', 'en');
-  newScript.setAttribute('data-loading', 'lazy');
-  newScript.setAttribute('crossOrigin', 'anonymous');
-  newScript.async = true;
+type GiscusElement = Omit<HTMLElement, "dataset"> & {
+  dataset: DOMStringMap & {
+    repo: `${string}/${string}`
+    repoId: string
+    category: string
+    categoryId: string
+    mapping: "url" | "title" | "og:title" | "specific" | "number" | "pathname"
+    strict: string
+    reactionsEnabled: string
+    inputPosition: "top" | "bottom"
+  }
+}
 
-  // Append the new script to the Giscus container
-  newGiscusContainer.appendChild(newScript);
+document.addEventListener("nav", () => {
+  const giscusContainer = document.querySelector(".giscus") as GiscusElement
+  if (!giscusContainer) {
+    return
+  }
+
+  const giscusScript = document.createElement("script")
+  giscusScript.src = "https://giscus.app/client.js"
+  giscusScript.async = true
+  giscusScript.crossOrigin = "anonymous"
+  giscusScript.setAttribute("data-loading", "lazy")
+  giscusScript.setAttribute("data-emit-metadata", "0")
+  giscusScript.setAttribute("data-repo", giscusContainer.dataset.repo)
+  giscusScript.setAttribute("data-repo-id", giscusContainer.dataset.repoId)
+  giscusScript.setAttribute("data-category", giscusContainer.dataset.category)
+  giscusScript.setAttribute("data-category-id", giscusContainer.dataset.categoryId)
+  giscusScript.setAttribute("data-mapping", giscusContainer.dataset.mapping)
+  giscusScript.setAttribute("data-strict", giscusContainer.dataset.strict)
+  giscusScript.setAttribute("data-reactions-enabled", giscusContainer.dataset.reactionsEnabled)
+  giscusScript.setAttribute("data-input-position", giscusContainer.dataset.inputPosition)
+
+  const theme = document.documentElement.getAttribute("saved-theme")
+  if (theme) {
+    giscusScript.setAttribute("data-theme", theme)
+  }
+
+  giscusContainer.appendChild(giscusScript)
+
+  document.addEventListener("themechange", changeTheme)
+  window.addCleanup(() => document.removeEventListener("themechange", changeTheme))
 })
