036cfcfe143eadf8770b1f7a7c19b1d475f8fbd0
MasssiveJuice <nicholas.h.rowell@gmail.com>
2024-08-29 22:24:32 +1200

    Fix: remove trailing slash from config `baseUrl`

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "morrowind-modding.github.io/",
+    baseUrl: "morrowind-modding.github.io",
     ignorePatterns: ["private", "**/templates/", ".obsidian"],
     defaultDateType: "created",
     theme: {
