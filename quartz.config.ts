a19f5ae4cc0c80ed5b7c2e661bd09b8d27891638
Jacky Zhao <j.zhao2k19@gmail.com>
2025-03-05 17:13:19 -0800

    fix(og-image): overflow ellipses in title and description

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "morrowind-modding.github.io",
     ignorePatterns: ["private", "**/templates/", ".obsidian"],
     defaultDateType: "modified",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
